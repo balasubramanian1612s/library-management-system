8ef6ec3e47ab2330b3da8088051538127ae870ce
Balasubramanian <61572479+companyofbala@users.noreply.github.com>
2021-10-28 13:32:17 +0530

    Application Init

--- lms_user/android/app/src/main/kotlin/com/example/lms_user/MainActivity.kt
+++ lms_user/android/app/src/main/kotlin/com/example/lms_user/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.lms_user
+package com.example.lms_student_user
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lms_user/lib/view/admin/admin_home_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:lms_student_user/model/hive/book_model.dart';
+import 'package:lms_student_user/model/side_bar_menu_model.dart';
+import 'package:lms_student_user/util/responsive.dart';
+import 'package:lms_student_user/view/admin/data_page.dart';
+import 'package:provider/provider.dart';
+
+import 'admin_side_bar.dart';
+
+class AdminHomeScreen extends StatefulWidget {
+  @override
+  _AdminHomeScreenState createState() => _AdminHomeScreenState();
+}
+
+class _AdminHomeScreenState extends State<AdminHomeScreen> {
+  Box<BookModel>? dataBox;
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final sidebarDecoration = BoxDecoration(
+      gradient: LinearGradient(
+          colors: [Color(0xff00B4DB), Color(0xff0083B0)],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter));
+
+  @override
+  void initState() {
+    dataBox = Hive.box<BookModel>('books');
+    print('opened at admin');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color(0xffced9de),
+      key: _scaffoldKey,
+      drawer: Container(
+        width: width * 0.7,
+        decoration: sidebarDecoration,
+        alignment: Alignment.topLeft,
+        child: AdminSideBar(),
+      ),
+      body: Builder(builder: (context) {
+        return SingleChildScrollView(
+          child: Consumer<SideBarMenuModel>(
+            builder: (context, menu, child) {
+              return Container(
+                height: height,
+                width: width,
+                child: Responsive(
+                    mobile: Row(
+                      children: [
+                        Container(
+                          width: width * 1,
+                          child: Stack(
+                            children: [
+                              getSelectedWidget(menu.isSelectedSidebar,
+                                  WidgetResponsive.mobile),
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: FloatingActionButton(
+                                  backgroundColor: Colors.transparent,
+                                  elevation: 0,
+                                  onPressed: () {
+                                    _scaffoldKey.currentState!.openDrawer();
+                                  },
+                                  child: Icon(
+                                    Icons.menu,
+                                    color: Color(0xff00B4DB),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    tablet: Row(
+                      children: [
+                        Container(
+                          width: width * 0.2,
+                          decoration: sidebarDecoration,
+                          alignment: Alignment.topLeft,
+                          child: AdminSideBar(),
+                        ),
+                        Container(
+                          width: width * 0.8,
+                          child: getSelectedWidget(
+                              menu.isSelectedSidebar, WidgetResponsive.tablet),
+                        ),
+                      ],
+                    ),
+                    desktop: Row(
+                      children: [
+                        Container(
+                          width: width * 0.2,
+                          decoration: sidebarDecoration,
+                          alignment: Alignment.topLeft,
+                          child: AdminSideBar(),
+                        ),
+                        Container(
+                          width: width * 0.8,
+                          child: getSelectedWidget(
+                              menu.isSelectedSidebar, WidgetResponsive.desktop),
+                        ),
+                      ],
+                    )),
+              );
+            },
+          ),
+        );
+      }),
+    );
+  }
+
+  Widget getSelectedWidget(int val, WidgetResponsive responsive) {
+    switch (val) {
+      case 1:
+        return DataPage();
+      default:
+        DataPage();
+    }
+    return Container();
+  }
+}
